a084fc08325cb010ec4676a5ab384e38223e36b1
Christian Rafael Camacho Dillon <chris_rcd@hotmail.com>
2018-01-15 01:29:36 -0500

    Agregar modelo vista controlador MVC

---
+++ canalesMVC/src/Controlador/Conexion.java
@@ -0,0 +1,53 @@
+package Controlador;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+
+    //------Atributos de la clase----------
+    String jdbcDriver = ("com.mysql.jdbc.Driver");//drivers
+    String jdbcSup = ("mysql");//base de datos
+    String jdbcHost = ("localhost");//servidor
+    String jdbcBd = ("canal");//nombre de la base de datos //no se si es de poner PersonaMVC
+    String jdbcUser = ("root");//usuario
+    String jdbcPwd = ("123456789");//pongo mi clave
+    Connection con = null;
+
+    public Conexion() {
+    }
+
+    public Connection conexionBD() {
+
+        try {
+            Class.forName(jdbcDriver);
+            String strcon = "jdbc:" + jdbcSup + "://" + jdbcHost + "/" + jdbcBd;
+            con = DriverManager.getConnection(strcon, jdbcUser, jdbcPwd);
+            System.out.println("Conexion Existosa");
+            return con;
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "mensaje fallo conexion", JOptionPane.ERROR_MESSAGE);
+            System.out.println("Driver Incorrectro" + ex.getMessage());
+            return null;
+        } catch (SQLException exsql) {
+            JOptionPane.showMessageDialog(null, exsql.getMessage(), "mensaje", JOptionPane.ERROR_MESSAGE);
+            System.out.println("BDD INCORRECTA" + exsql.getMessage());
+            return null;
+        }
+    }
+
+    public void mensaje() {
+        JOptionPane.showMessageDialog(null, "mensaje", "contenido", 3);
+    }
+
+    public void desconectar() {
+        try {
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
